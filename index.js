40fd0bdf396af658b09642bcd0a8b7d65f200f1d
Manuel Olivera <manuod95@gmail.com>
2023-11-02 05:43:21 +0100

    Condicional y ciclo realizados

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-alert("Bienvenido al censo digital. Respondé las preguntas :D")
-let nombre = prompt("Ingrese su nombre:");
-let apellido = prompt("Ingrese su apellido:");
-let esHombre = confirm("Apretá aceptar si sos hombre. Si sos mujer, tocá cancelar");
-let sexo
-let documento = prompt("Ingrese su número de documento:");
-let edad = parseInt(prompt("Ingrese su edad:"));
+alert("Bienvenido al censo digital. Respondé las preguntas :D");
+const cantidadFamiliares = parseInt(prompt("Cantidad de familiares que van a contestar (en números)"));
 
-if (esHombre) {
-    sexo = "¡Bienvenido!"
-} else {
-    sexo = "¡Bienvenida!"
-};
+for (let i = 0; i < cantidadFamiliares; i++) {
+    let nombre = prompt("Ingrese su nombre:");
+    let apellido = prompt("Ingrese su apellido:");
+    let esHombre = confirm("Apretá aceptar si sos hombre. Si sos mujer, tocá cancelar");
+    let sexo;
 
-if (edad >= 0 && edad <= 12) {
-    alert("Hey " + nombre + " " + apellido + ", " + sexo + " Estás en la categoría: niño.");
-} else if (edad >= 13 && edad <= 18) {
-    alert("Hey " + nombre + " " + apellido + ", " + sexo + " Estás en la categoría: adolescente.");
-} else if (edad >= 19 && edad <= 29) {
-    alert("Hey " + nombre + " " + apellido + ", " + sexo + " Estás en la categoría: joven.");
-} else if (edad >= 30 && edad <= 69) {
-    alert("Hey " + nombre + " " + apellido + ", " + sexo + " Estás en la categoría: adulto.");
-} else {
-    alert("Hey " + nombre + " " + apellido + ", " + sexo + " Estás en la categoría: adulto mayor.");
+    if (esHombre) {
+        sexo = "¡Bienvenido!";
+    } else {
+        sexo = "¡Bienvenida!";
+    }
+
+    let documento = prompt("Ingrese su número de documento:");
+    let edad = parseInt(prompt("Ingrese su edad:"));
+
+    if (edad >= 0 && edad <= 12) {
+        alert("Hey " + nombre + " " + apellido + ", " + sexo + " Estás en la categoría: niño.");
+    } else if (edad >= 13 && edad <= 18) {
+        alert("Hey " + nombre + " " + apellido + ", " + sexo + " Estás en la categoría: adolescente.");
+    } else if (edad >= 19 && edad <= 29) {
+        alert("Hey " + nombre + " " + apellido + ", " + sexo + " Estás en la categoría: joven.");
+    } else if (edad >= 30 && edad <= 69) {
+        alert("Hey " + nombre + " " + apellido + ", " + sexo + " Estás en la categoría: adulto.");
+    } else {
+        alert("Hey " + nombre + " " + apellido + ", " + sexo + " Estás en la categoría: adulto mayor.");
+    }
 }
